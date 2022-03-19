4c27a083b13d63c5b23f4d96e26a3700edc4e5fe
Duc Nguyen <minhduc140583@gmail.com>
2022-03-20 00:52:16 +0700

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LayoutComponent from './components/Layout';
+import AboutPage from './pages/About';
+import HomePage from './pages/Home';
 import './App.css';
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about">
+          <Route index element={<AboutPage />} />
+          <Route path=":number" element={<AboutPage />} />
+        </Route>
+        <Route path="layout" element={<LayoutComponent />}>
+          <Route index element={<AboutPage />} />
+          <Route path=":number" element={<AboutPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +39,7 @@ function App() {
       </header>
     </div>
   );
+  */
 }
 
 export default App;
