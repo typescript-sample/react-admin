76ba50d8a57805af83ebfa1824a1d4bffb9bbd9f
Duc Nguyen <minhduc140583@gmail.com>
2025-01-12 19:29:24 +0700

    Refactor theme, before remove all view-container in theme

--- src/user/user-view.tsx
+++ src/user/user-view.tsx
@@ -30,34 +30,32 @@ export const UserView = () => {
     navigate(-1)
   }
   return (
-    <div className="view-container">
-      <form id="userForm" name="userForm" model-name="user" ref={refForm as any}>
-        <header className="view-header">
-          <button type="button" id="btnBack" name="btnBack" className="btn-back" onClick={back} />
-          <h2 className="view-title">{resource.user}</h2>
-        </header>
-        <div className="row">
-          <dl className="data-list row">
-            <dt className="col s6 l3">{resource.user_id}</dt>
-            <dd className="col s6 l9">{user?.userId}</dd>
-            <dt className="col s6 l3">{resource.username}</dt>
-            <dd className="col s6 l9">{user?.username}</dd>
-            <dt className="col s6 l3">{resource.person_title}</dt>
-            <dd className="col s6 l9">{user?.title}</dd>
-            <dt className="col s6 l3">{resource.display_name}</dt>
-            <dd className="col s6 l9">{user?.displayName}</dd>
-            <dt className="col s6 l3">{resource.phone}</dt>
-            <dd className="col s6 l9">{formatPhone(user?.phone)}</dd>
-            <dt className="col s6 l3">{resource.email}</dt>
-            <dd className="col s6 l9">{user?.email}</dd>
-          </dl>
-        </div>
-        <footer className="view-footer">
-          <button type="submit" id="btnClose" name="btnClose" onClick={back}>
-            {resource.close}
-          </button>
-        </footer>
-      </form>
-    </div>
+    <form id="userForm" name="userForm" className="form" model-name="user" ref={refForm as any}>
+      <header className="view-header">
+        <button type="button" id="btnBack" name="btnBack" className="btn-back" onClick={back} />
+        <h2 className="view-title">{resource.user}</h2>
+      </header>
+      <div className="row">
+        <dl className="data-list row">
+          <dt className="col s6 l3">{resource.user_id}</dt>
+          <dd className="col s6 l9">{user?.userId}</dd>
+          <dt className="col s6 l3">{resource.username}</dt>
+          <dd className="col s6 l9">{user?.username}</dd>
+          <dt className="col s6 l3">{resource.person_title}</dt>
+          <dd className="col s6 l9">{user?.title}</dd>
+          <dt className="col s6 l3">{resource.display_name}</dt>
+          <dd className="col s6 l9">{user?.displayName}</dd>
+          <dt className="col s6 l3">{resource.phone}</dt>
+          <dd className="col s6 l9">{formatPhone(user?.phone)}</dd>
+          <dt className="col s6 l3">{resource.email}</dt>
+          <dd className="col s6 l9">{user?.email}</dd>
+        </dl>
+      </div>
+      <footer className="view-footer">
+        <button type="submit" id="btnClose" name="btnClose" onClick={back}>
+          {resource.close}
+        </button>
+      </footer>
+    </form>
   )
 }
